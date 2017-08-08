67a6b049f4d3b7cef278b62ad7316acf92c3c87a
Damian Wtorek <damian.wtr@gmail.com>
2017-08-08 17:34:35 +0200

    added redirect option

--- src/main/java/com/damianck/pl/controller/TweetController.java
+++ src/main/java/com/damianck/pl/controller/TweetController.java
@@ -7,8 +7,11 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +21,22 @@ public class TweetController {
     private Twitter twitter;
 
     @RequestMapping("/")
+    public String home() {
+        return "searchPage";
+    }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String search = request.getParameter("search");
+        if (search.toLowerCase().contains("tusk")){
+            redirectAttributes.addFlashAttribute("error","Wpisz cos innego!");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute(search);
+        return "redirect:result";
+    }
+
+    @RequestMapping("/result")
     public String hello(@RequestParam(defaultValue = "spring social") String search, Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
         List<Tweet> tweets = searchResults.getTweets();

2c707c050b3b982fb4edef5787c4d7c6a1e68764
Damian Wtorek <damian.wtr@gmail.com>
2017-08-08 18:47:17 +0200

    added ModelAttribute for date

--- src/main/java/com/damianck/pl/controller/ProfileController.java
+++ src/main/java/com/damianck/pl/controller/ProfileController.java
@@ -1,13 +1,30 @@
 package com.damianck.pl.controller;
 
+import com.damianck.pl.date.USLocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
 
 @Controller
 public class ProfileController {
     @RequestMapping("/profile")
-    public String displayProfile() {
+    public String displayProfile(ProfileForm profileForm) {
         return "profile/profilePage";
     }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String saveProfile(ProfileForm profileForm) {
+        System.out.println("pomyslnie zapisano profil " + profileForm);
+        return "profile/profilePage";
+    }
+
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+        return USLocalDateFormatter.getPattern(locale);
+    }
+
 
 }

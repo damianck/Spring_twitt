a1803c4ec1c54851022f12a1034f347b224906eb
Damian Wtorek <damian.wtr@gmail.com>
2017-08-09 16:23:09 +0200

    added upload

--- src/main/java/com/damianck/pl/SpringMvcApplication.java
+++ src/main/java/com/damianck/pl/SpringMvcApplication.java
@@ -1,9 +1,12 @@
 package com.damianck.pl;
 
+import com.damianck.pl.config.PictureUploadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({PictureUploadProperties.class})
 public class SpringMvcApplication {
 
 	public static void main(String[] args) {
